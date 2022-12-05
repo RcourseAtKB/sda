cf7f2f5859d904eed8b7cef3f36a688449fc94ba
Thomas Maltesen <thomas.maltesen@protonmail.com>
2022-12-05 15:03:34 +0100

    Update helpreadshak.R

--- R/helpreadshak.R
+++ R/helpreadshak.R
@@ -2,14 +2,15 @@ helpreadshak <-  function() {
   SHAK <- suppressMessages(read_delim("https://sor-filer.sundhedsdata.dk/sor_produktion/data/shak/shakregion/SHAKregion.txt",
                                       delim = "\t", escape_double = FALSE,
                                       col_names = FALSE, trim_ws = TRUE, locale = locale(encoding = "latin1")))
-  colnames(SHAK) <- c("Shak", "Place", "Valid from", "Valid to", "X5", "Area", "X7", "X8")
+  colnames(SHAK) <- c("K_SGH", "V_SGHNAVN", "K_FRADTO", "D_TILDTO", "C_SGHTYPE", "C_INSTART", "C_REGION", "C_SORID")
+  
   SHAK <- data.frame(SHAK)
   SHAK <- SHAK %>% mutate(Region = NA_character_,
-                          Region = if_else(X7 %in% 1081, "Nordjylland", Region),
-                          Region = if_else(X7 %in% 1082, "Midtjylland", Region),
-                          Region = if_else(X7 %in% 1083, "Syddanmark", Region),
-                          Region = if_else(X7 %in% 1084, "Hovedstaden", Region),
-                          Region = if_else(X7 %in% 1085, "Sj\u00e6lland", Region))
-  SHAK$X7 <- NULL
+                          Region = if_else(C_REGION %in% 1081, "Nordjylland", Region),
+                          Region = if_else(C_REGION %in% 1082, "Midtjylland", Region),
+                          Region = if_else(C_REGION %in% 1083, "Syddanmark", Region),
+                          Region = if_else(C_REGION %in% 1084, "Hovedstaden", Region),
+                          Region = if_else(C_REGION %in% 1085, "Sj\u00e6lland", Region))
+  SHAK$C_REGION <- NULL
   return(SHAK)
 }
