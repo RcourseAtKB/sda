19295c97d4b1645a9ea3a1e2564c187337d7de97
Thomas Maltesen <thomas.maltesen@protonmail.com>
2022-12-09 15:33:53 +0100

    Update lookatSHAK.R

--- R/lookatSHAK.R
+++ R/lookatSHAK.R
@@ -1,12 +1,16 @@
 lookatSHAK <- function(x) {
-  if(is.character(x)){
-    if(nchar(x)>3){
-      DF <- helpreadshak()
-      return(DF[DF$K_SGH %in% substr(x, 1, 4),])
+  if(is.character(warnErrList(suppressWarnings(helpreadshak()))){
+    warnErrList(suppressWarnings(helpreadshak()))
+   } else {
+    if(is.character(x)){
+      if(nchar(x)>3){
+        DF <- helpreadshak()
+        return(DF[DF$K_SGH %in% substr(x, 1, 4),])
+      } else{
+        warning("The string has to be a least 4 characters.")
+      }
     } else{
-      warning("The string has to be a least 4 characters.")
+      warning("Input has to be a character.")
     }
-  } else{
-    warning("Input has to be a character.")
   }
 }
