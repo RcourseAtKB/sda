4d4416e1b4a3a21c0a156d2a6b8f495a9ca7437e
Thomas Maltesen <thomas.maltesen@protonmail.com>
2022-12-09 15:25:29 +0100

    Update helpreadshak.R

--- R/helpreadshak.R
+++ R/helpreadshak.R
@@ -1,6 +1,6 @@
 helpreadshak <-  function() {
   if(url.exists("https://sor-filer.sundhedsdata.dk/sor_produktion/data/shak/shakregion/SHAKregion.txt")){
-    SHAK <- suppressMessages(read_delim("https://sor-filer.sundhedsdata.dk/sor_produktion/data/shak/shakregion/SHAKregion.txt",
+    SHAK <- suppressMessages(read_delim("https://sor-filer.sundhedsdata.dk/sor_produktion/data/shak/shakregion/SHAKregion .txt",
                                         delim = "\t", escape_double = FALSE,
                                         col_names = FALSE, trim_ws = TRUE, locale = locale(encoding = "latin1")))
     colnames(SHAK) <- c("K_SGH", "V_SGHNAVN", "K_FRADTO", "D_TILDTO", "C_SGHTYPE", "C_INSTART", "C_REGION", "C_SORID")
@@ -19,5 +19,5 @@ helpreadshak <-  function() {
     SHAK$C_REGION <- NULL
     SHAK$C_SGHTYPE <- NULL
     return(SHAK)
-  } else {warning("The link does not work.")}
+  } else {warning("The link does not work. Write to thomas.maltesen@proton.me")}
 }
